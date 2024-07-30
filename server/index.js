e05840bbdccab27f67768aade37a3fd1da4a82b4
Himanshu Singh Bhandari <himanshubhandari4675@gmail.com>
2024-07-30 19:53:33 +0530

    vercel file

--- server/index.js
+++ server/index.js
@@ -16,18 +16,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
 
 app.use(express.json());
 app.use(cookieParser());
