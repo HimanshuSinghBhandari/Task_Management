cd94006838a2e2a20fd24513c0d8f97f1c8151cd
Himanshu Singh Bhandari <himanshubhandari4675@gmail.com>
2024-07-30 10:31:55 +0530

    adding server files

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const dbConnect = require("./lib/dbConnect");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const TaskRouter = require("./routes/TaskRouter")
-
+require("dotenv").config();
 const app = express();
 
 // CORS configuration
@@ -15,6 +15,19 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 require("dotenv").config();
